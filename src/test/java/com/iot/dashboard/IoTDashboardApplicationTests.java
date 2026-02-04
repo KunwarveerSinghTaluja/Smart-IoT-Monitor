dbf6b7c023956b11d147b5b979047889a9434236
KunwarveerSinghTaluja <kunwarvs.chd@gmail.com>
2026-02-04 19:19:45 +0530

    Update IoTDashboardApplicationTests.java

--- src/test/java/com/iot/dashboard/IoTDashboardApplicationTests.java
+++ src/test/java/com/iot/dashboard/IoTDashboardApplicationTests.java
@@ -1,13 +1,1 @@
-package com.iot.dashboard;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class IoTDashboardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
